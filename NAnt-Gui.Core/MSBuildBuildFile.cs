38dbe696897cc87e92eaee4542803bb25db8dff1
swoogan <devnull@localhost>
2005-11-29 17:23:27 +0000

    Added stubbed out derived build files.

---
+++ NAnt-Gui.Core/MSBuildBuildFile.cs
@@ -0,0 +1,16 @@
+
+namespace NAntGui.Core
+{
+	/// <summary>
+	/// Summary description for MSBuildBuildFile.
+	/// </summary>
+	public class MSBuildBuildFile : BuildFile
+	{
+		public MSBuildBuildFile()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
---
+++ NAnt-Gui.Core/NAntBuildFile.cs
@@ -0,0 +1,16 @@
+
+namespace NAntGui.Core
+{
+	/// <summary>
+	/// Summary description for NantBuildFile.
+	/// </summary>
+	public class NantBuildFile : BuildFile
+	{
+		public NantBuildFile()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
